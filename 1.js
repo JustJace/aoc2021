801e61f36e49b49e1613461b4a3f04640f5e88f0
Jace Brewster <jacebrewster@gmail.com>
2021-12-01 08:51:53 -0800

    cleanup

--- 1.js
+++ 1.js
@@ -6,7 +6,7 @@ const lines = unpackLines(input);
 let part1 = 0;
 
 for (let i = 0; i < lines.length - 1 ; i++)
-    if (lines[i+1]>lines[i]) 
+    if (lines[i+1] > lines[i]) 
         part1++;
 
 const groups = [];
@@ -16,9 +16,8 @@ for (let i = 0; i < lines.length - 2; i++)
 
 let part2 = 0;
 
-for (let i = 0; i < groups.length - 1; i++) {
+for (let i = 0; i < groups.length - 1; i++)
     if (groups[i+1] > groups[i])
         part2++;
-}
 
 console.log({ part1, part2 });
